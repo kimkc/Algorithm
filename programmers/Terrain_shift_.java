9ad17d7f65af7cf58095077d6441a5d14ee1acd2
kimkc <42633180+kimkc@users.noreply.github.com>
2020-05-22 13:43:11 +0900

    Create  Terrain_shift_.java

---
+++ programmers/Terrain_shift_.java
@@ -0,0 +1,16 @@
+class Solution {
+    static int[] ny = {1, -1, 0, 0};
+    static int[] nx = {0, 0, 1, -1};
+    public int solution(int[][] land, int height) {
+        int ahttps:
+        return answer;
+    }
+}
+/*
+2020.5.22
+12:58~13:10분 못품
+설치비용을 저장하며, 가는 곳을 visited로 표시하며, !visited를 갈 때, 상하좌우를 중 설치비용이 제일 낮은 곳으로 이동한다.같다면 아무곳으로 가도 된다. => 전혀 다른 접근/ 후에 전체 탐색과 백트래킹을 하며 찾을 수 있는 방법이 가능한지 알아보기
+코드 공부 13:10~13:42
+https://yabmoons.tistory.com/470
+
+*/
